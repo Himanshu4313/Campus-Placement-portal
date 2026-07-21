24ef4f3a58a80dbd145481ddc59f96e9b9fbc5f4
Aman Kumar <atulamanraj55@gmail.com>
2026-07-22 02:02:16 +0530

    Update:- Update some existing code in client side

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 'https://campus-placement-portal-v079.onrender.com/api'
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL ||  'https://campus-placement-portal-v079.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
